a7a14040ede7c14a223cc848ba9eb4512406d778
Abhinav Srivastava <abhinavsrivastava170@gmail.com>
2020-02-06 15:22:30 +0530

    edited again

--- hungry.py
+++ hungry.py
@@ -1,7 +1,13 @@
+Learn more or give us feedback
 hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat")
+    print("eat 2")
+    print("eat 3")
+    print("eat 4")
 else:
-    print("do something")
-
-   // This code is edited.
+    thirsty = input("are you thirsty?")
+    if thirsty == "yes":
+        print("drink")
+        print("drink 2")
+// This code has been edited.
