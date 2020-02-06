abb84971f287527c35ba7b87741d99306ead46f7
Karan Gandhi <gandhi.karan24@gmail.com>
2020-02-06 14:58:05 +0530

    Fifth master

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry = input("are you hungry?")
 if hungry == "yes":
-    print("eat"
+    print("eat")
     print("eat 2")
     print("eat 3")
+    print("eat 4")
 else:
     print("do something")
